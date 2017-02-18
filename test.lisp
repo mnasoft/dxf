3224b101e9e4ca8549ef607a2662f8f714f4f65e
Nick Matvyeyev <mnasoft@gmail.com>
2017-02-18 23:58:51 +0200

    2017-2-18 23:58:50 MNASOFT-01

--- test.lisp
+++ test.lisp
@@ -16,9 +16,11 @@
    (make-instance 'db-arc)
    (make-instance 'db-arc :center (vector 25 15 0) :radius 18.6 :entity-color 70 :start-angle (* 1.9999 pi) :end-angle (* 3.999 pi))
    (make-instance 'db-text :position-point (vector 30 20 0) :rotation (* *degree-to-radian* 120) :text-string "This is the TEXT string!" :width-factor 1/3 :oblique (* *degree-to-radian* 15))
-   (make-instance 'Db-Ellipse)
-   (make-instance 'Db-Ellipse :center (vector 10 20 0) :entity-color 3 :entity-layer "Ellipses" :major-axis (vector 2 0 0) :radius-ratio 0.5D0 :start-param (* *degree-to-radian* 120) :end-param (* *degree-to-radian* 390))
-   (make-instance 'Db-Ray :base-point (vector 51 65 0) :unit-dir (vector 1 0 0))
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;(make-instance 'Db-Ellipse)
+   ;;(make-instance 'Db-Ellipse :center (vector 10 20 0) :entity-color 3 :entity-layer "Ellipses" :major-axis (vector 2 0 0) :radius-ratio 0.5D0 :start-param (* *degree-to-radian* 120) :end-param (* *degree-to-radian* 390))
+   ;;(make-instance 'Db-Ray)
+   ;;(make-instance 'Db-Ray :base-point (vector 51 65 0) :unit-dir (vector 1 0 0))
    ))
 
 (defun test-bin ()
@@ -30,7 +32,6 @@
   (close *o*))
 
 (test-bin)
-
 
 (defun test-txt ()
   (defparameter *o* (open (concatenate 'string *dxf-path* "12345-txt.dxf" )  :direction :output :if-exists :supersede))
