39cb7988639b8057c4d6b67e08a75d0618b2192d
Nick Matvyeyev <mnasoft@gmail.com>
2022-03-30 15:03:08 +0300

    30 мар 2022 г. 12:03:07

--- src/out/test.lisp
+++ src/out/test.lisp
@@ -22,3 +22,10 @@
   (with-open-file (dxf (concatenate 'string dxf::*dxf-path* "101_txt-sections.dxf")
                        :direction :output :if-exists :supersede)
     (txt-sections data dxf)))
+
+(let ((data
+        (dxf/in/txt:dxf-in-t-fname 
+           (concatenate 'string dxf::*dxf-path* "Line_01.dxf"))))
+  (with-open-file (dxf (concatenate 'string dxf::*dxf-path* "_Line_01.dxf")
+                       :direction :output :if-exists :supersede)
+    (txt-sections data dxf)))
