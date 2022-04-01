6ef0edb5f79d58b30e7cdc76f6bf08816ca47667
Nick Matvyeyev <mnasoft@gmail.com>
2022-04-01 17:23:55 +0300

    1 апр 2022 г. 14:23:55

--- src/in/bin/bin.lisp
+++ src/in/bin/bin.lisp
@@ -26,11 +26,29 @@
 (in-package #:dxf/in/bin)
 
 (defun read-string (stream)
-  "@b(Описание:) функция @b(read-string) выполняет считывание строки
-  из потока @b(stream).
+  "@b(Описание:) функция @b(read-string) возвращает строку, считывамую
+ из потока @b(stream). В потоке @b(stream) строка должна храниться как
+ нуль-терминированная (заканчиваться на символ #\Nul). Указатель
+ чтения потока @(stream) после выполнения функции указывет на символ,
+ следующий за нуль-терминатором. Нуль-терминатор в возвращаемую
+ строку не попадает.
 
- @b(Пример использования:)
+ @b(Пример использования:) 
 @begin[lang=lisp](code)
-
+ (let ((s-01 (format nil \"~A~C~C~C~C\" \"AutoCAD Binary DXF\" #\\CR #\\LF #\\SUB #\\NUL))
+       (s-02 (format nil \"~A~C\" \"Строка - 02\" #\\NUL)))
+   (with-open-file (dxf (concatenate 'string dxf::*dxf-path* \"in-bin.dxf\")
+                        :direction :output :if-exists :supersede
+                        :element-type 'unsigned-byte)
+     (write-sequence (babel:string-to-octets s-01) dxf)
+     (write-sequence (babel:string-to-octets s-02) dxf))
+   (with-open-file (dxf (concatenate 'string dxf::*dxf-path* \"in-bin.dxf\")
+     :element-type 'unsigned-byte)
+   (format t \"~S~%~S\" (read-string dxf) (read-string dxf))))
 @end(code)"
-  )
+  (do ((bt (read-byte stream) (read-byte stream))
+     (i 0 (1+ i))
+     (rez nil (push bt rez)))
+    ((= bt 0) (babel:octets-to-string
+               (map '(vector (unsigned-byte 8))
+                    #'(lambda (el) el) (nreverse rez))))))
